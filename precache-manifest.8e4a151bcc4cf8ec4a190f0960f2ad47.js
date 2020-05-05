de43eae848aa731894b797f8a76e344e4c8334bd
shankartocode <contact@krgowrisankar.in>
2020-05-05 14:40:49 +0530

    Please enter the commit message for your changes. Lines starting ii# with '#' will be ignored, and an empty message aborts the commit.

---
+++ precache-manifest.8e4a151bcc4cf8ec4a190f0960f2ad47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f8c3d268daa0f09b3329db4b01dec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2858a0f5a48609a7e912",
+    "url": "/static/css/2.4ded7582.chunk.css"
+  },
+  {
+    "revision": "7d0fc36327758bed0eb6",
+    "url": "/static/css/main.1cdaad28.chunk.css"
+  },
+  {
+    "revision": "2858a0f5a48609a7e912",
+    "url": "/static/js/2.03a64bcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.03a64bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0fc36327758bed0eb6",
+    "url": "/static/js/main.5338d02f.chunk.js"
+  },
+  {
+    "revision": "366a180f8bec8859927f",
+    "url": "/static/js/runtime-main.b15d8666.js"
+  }
+]);
